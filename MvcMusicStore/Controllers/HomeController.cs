b47a0f886c1ed0498f3a24131f7e89d0256848b8
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-04-29 13:01:34 +0530

    View returns the method of controller

--- MvcMusicStore/Controllers/HomeController.cs
+++ MvcMusicStore/Controllers/HomeController.cs
@@ -6,12 +6,15 @@ using System.Web.Mvc;
 
 namespace MvcMusicStore.Controllers
 {
+    // Controller is the class with methods.
     // View maps the Controller with the common name i.e. Home in this case
     public class HomeController : Controller
     {
         // Index is the method which is mapped with the .cshtml file in View->Home dir
         public ActionResult Index()
         {
+            // View will return the Index method of Home controller 
+            // and find its view because the name matches. 
             return View();
         }
 
