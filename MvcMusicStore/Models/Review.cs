c65484c35027c47f24374757bf559a92b24d158f
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-05-02 16:29:24 +0530

    ADD:  Review class and properties

---
+++ MvcMusicStore/Models/Review.cs
@@ -0,0 +1,13 @@
+﻿namespace MvcMusicStore.Models
+{
+    public class Review
+    {
+        public int ReviewID { get; set; }
+
+        public Album Album { get; set; }
+
+        public string Contents { get; set; }
+
+        public string ReviewerEmail { get; set; }
+    }
+}
