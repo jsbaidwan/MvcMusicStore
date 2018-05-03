a765eaa7bf27e6056db9ae31bc45a7a4472ed996
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-05-03 16:15:36 +0530

    READD: Review class in Model

---
+++ MvcMusicStore/Models/Review.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class Review
+    {
+        public int ReviewID { get; set; }
+
+        public Album Album { get; set; }
+
+        public string Contents { get; set; }
+
+        public string ReviewerEmail { get; set; }
+    }
+}
