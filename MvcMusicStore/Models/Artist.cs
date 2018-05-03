649183ea7b2275d0848baa905b3a3ac8031a4483
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-05-03 16:17:04 +0530

    READD: Artist class in Model

---
+++ MvcMusicStore/Models/Artist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class Artist
+    {
+        public int ArtistID { get; set; }
+
+        public string Title { get; set; }
+
+        public List<Album> Albums { get; set; }
+    }
+}
